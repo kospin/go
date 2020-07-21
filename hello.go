f6ef3ae6072dc3c553a5ac8981dc06180831bb2a
kospin <honeymoon0624@gmail.com>
2020-07-21 10:05:32 +0800

    test

--- hello.go
+++ hello.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math"
 	"runtime"
+	"time"
 )
 
 func add(x, y int) int {
@@ -21,12 +22,15 @@ func split(合計 int) (x, y int) {
 }
 
 func sqrt(x float64) float64 {
-	t, z := 1, float64(1)
-	for t <= 10 {
-		z = z - (z*z-x)/(2*z)
-		t++
+	z := float64(1)
+	for t := 1; t <= 10; t++ {
+		z = z - (平方(z)-x)/(2*z)
 	}
 	return z
+}
+
+func 平方(x float64) float64 {
+	return x * x
 }
 
 var i int
@@ -57,7 +61,41 @@ func main() {
 	case "linux":
 		fmt.Println("Linux.")
 	default:
-		fmt.Printf("%s.", runtime.GOOS)
+		fmt.Printf("%s.\n", runtime.GOOS)
 	}
+
+	defer fmt.Println("自爆...") //延遲啊...好像很有用
+	defer fmt.Println("我先!!")  //後進先出
+
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("早安")
+	default:
+		fmt.Println("晚安")
+	}
+	var p *int //指標!!
+	n := 42
+	p = &n
+	fmt.Println(*p) //沒有指標運算,那有什麼用?
+
+	type 向量 struct {
+		X int
+		Y int
+	}
+
+	fmt.Println(向量{1, 2})
+	v := 向量{3, 4}
+	w := &v
+	w.X = 1e9 //w.X 等同v.X
+	fmt.Println(v, w)
+
+	a := [2]string{"hello", "world"}
+	fmt.Println(a)
+
+	primes := []int{2, 3, 5, 7, 11, 13}
+	s := primes[1:4]
+	s[0] = 23
+	fmt.Println(s, primes) //切片不是新的矩陣
 
 }
